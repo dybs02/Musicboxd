0a0db38919f0695efa2d46df59635c38acc04ad1
Mateusz Dybala <matidybala@gmail.com>
2024-10-17 20:10:11 +0200

    modify navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,29 +1,73 @@
 <script setup lang="ts">
-import { RouterLink, useRouter } from 'vue-router'
-import HamburgerMenu from '@/assets/icons/HamburgerMenu.vue';
-import MagnifyingGlass from '@/assets/icons/MagnifyingGlass.vue';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import AutoComplete from 'primevue/autocomplete';
+import Button from 'primevue/button';
+import Menubar from 'primevue/menubar';
+
 
 
 const router = useRouter();
+const search_value = ref('');
+const nav_items = ref([
+  {
+      label: 'Home',
+      icon: 'pi pi-home',
+      command: () => {
+          router.push({name: 'home'});
+      }
+  },
+  {
+      label: 'Profile',
+      icon: 'pi pi-star',
+      command: () => {
+          router.push({name: 'user', params: {id: 123}});
+      }
+  },
+  {
+      label: 'About',
+      icon: 'pi pi-envelope',
+      command: () => {
+          router.push({name: 'about'});
+      }
+  }
+]);
+const suggest_items = ref(['']);
 
-const goHome = () => {
-  router.push('/');
+
+const suggest_search = (event: any) => {
+  console.log(event.query);
+  suggest_items.value = [
+    'Apple',
+    'Banana',
+    'Orange'
+  ];
+}
+
+const login = () => {
+  console.log('Login');
 }
 
 </script>
 
 <template>
-  <div class="flex h-10 py-2 px-4 bg-midnight">
-    <HamburgerMenu class="fill-snow md:hidden"/>
-
-    <div @click="goHome" class="px-2 my-auto cursor-pointer">
-      Home
-    </div>
-    
-    <div class="w-full"></div>
-    
-    <MagnifyingGlass class="fill-snow"/>
-  </div>
+  <Menubar :model="nav_items">
+    <template #start>
+      <div class="w-10 h-10">
+        <img alt="Vue logo" src="https://picsum.photos/640" />
+      </div>
+    </template>
+    <template #end>
+      <div class="flex items-center gap-2">
+        <AutoComplete placeholder="Search"
+                      v-model="search_value"
+                      :suggestions="suggest_items"
+                      @complete="suggest_search"
+                      />
+        <Button label="Login" severity="secondary" @click="login" />
+      </div>
+    </template>
+  </Menubar>
 </template>
 
 <style scoped>
