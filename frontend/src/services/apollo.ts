72d28400334f16782622110b75c15b6c653de5f8
Mateusz Dybala <matidybala@gmail.com>
2025-01-22 15:39:45 +0100

    set credentials include

--- frontend/src/services/apollo.ts
+++ frontend/src/services/apollo.ts
@@ -23,6 +23,7 @@ const authMiddleware = new ApolloLink((operation, forward) => {
 const apolloClient = new ApolloClient({
   link: concat(authMiddleware, httpLink),
   cache: new InMemoryCache(),
+  credentials: 'include',
 });
 
 
